93f4672038ebb2fe7f9b079bc30dcec9f5cd1119
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-09-29 11:59:27 -0700

    Code review feedback.

--- test_app.py
+++ test_app.py
@@ -23,10 +23,10 @@ class BoggleAppTestCase(TestCase):
         with app.test_client() as client:
             response = client.get('/')
             html = response.get_data(as_text=True)
+
             self.assertEqual(response.status_code, 200)
             self.assertIn('<title>Boggle</title>', html)
-            ...
-            # test that you're getting a template
+            self.assertIn('Homepage Template', html)
 
     def test_api_new_game(self):
         """Test starting a new game."""
